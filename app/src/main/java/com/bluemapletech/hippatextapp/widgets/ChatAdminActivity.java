57103789fa56e11ecce30e5a9eaecfc7ecb86f5b
vigneshpandi <vignesh08993@gmail.com>
2016-11-15 17:30:04 +0530

    include the single chat in admin role

--- app/src/main/java/com/bluemapletech/hippatextapp/widgets/ChatAdminActivity.java
+++ app/src/main/java/com/bluemapletech/hippatextapp/widgets/ChatAdminActivity.java
@@ -1,10 +1,106 @@
 package com.bluemapletech.hippatextapp.widgets;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.bluemapletech.hippatextapp.R;
+import com.bluemapletech.hippatextapp.adapter.PageEmployeeBaseAdpter;
+import com.bluemapletech.hippatextapp.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kumaresan on 20-10-2016.
  */
 
 public class ChatAdminActivity extends Fragment{
+
+    private ListView listview;
+    ArrayList empList = new ArrayList();
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase fireBaseDatabase;
+    private String loggedINCompany;
+    private String loggedINEmail;
+    private String loggedINChatPin;
+    private static final String TAG = IntraChatEmployeeTabActivity.class.getCanonicalName();
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.accepted_admin_tab_fragment, container, false);
+
+        listview = (ListView) rootView.findViewById(R.id.accepted_admin_tab_fragment);
+
+        fireBaseDatabase = FirebaseDatabase.getInstance();
+        final User user = new User();
+        checkCompanyExistence();
+        DatabaseReference dataReference = fireBaseDatabase.getReference().child("userDetails");
+        dataReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user;
+                List<User> userObj = new ArrayList<User>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.d(TAG, "Snapshot value: " + snapshot.toString());
+                    user = new User();
+                    user.setCompanyName(snapshot.child("companyName").getValue(String.class));
+                    user.setEmpId(snapshot.child("employeeId").getValue(String.class));
+                    user.setPassword(snapshot.child("password").getValue(String.class));
+                    user.setRole(snapshot.child("role").getValue(String.class));
+                    user.setAuth(snapshot.child("auth").getValue(String.class));
+                    user.setUserName(snapshot.child("emailAddress").getValue(String.class));
+                    user.setSenderId(snapshot.child("senderId").getValue(String.class));
+                    user.setPushNotificationId(snapshot.child("pushNotificationId").getValue(String.class));
+                    if (user.getRole().matches("user") && user.getAuth().matches("1")&& loggedINCompany.matches(user.getCompanyName()) && !loggedINEmail.matches(user.getUserName())) {
+                        userObj.add(user);
+                    }
+                }
+                if(getActivity() !=null) {
+                    listview.setAdapter(new PageEmployeeBaseAdpter(getActivity(), userObj, loggedINEmail, loggedINChatPin));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return rootView;
+    }
+
+
+    public void checkCompanyExistence() {
+        fireBaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser logged = firebaseAuth.getCurrentUser();
+        Log.d(TAG, "Logged in user information's: " + logged.getEmail());
+        String reArrangeEmail = logged.getEmail().replace(".", "-");
+        DatabaseReference dataReferences = fireBaseDatabase.getReference().child("userDetails").child(reArrangeEmail);
+        dataReferences.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                loggedINCompany = (String) dataSnapshot.child("companyName").getValue();
+                loggedINEmail = (String) dataSnapshot.child("emailAddress").getValue();
+                loggedINChatPin = (String) dataSnapshot.child("chatPin").getValue();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
