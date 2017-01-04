1e218e7a592aab9d33a35297ef7515ea6906ce4a
vikash5 <vikashche@gmail.com>
2017-01-04 17:38:05 +0530

    changes in viewGroupDetail.java,activity_view_group_detail,view_groupingdetail.xml

--- app/src/main/java/com/bluemapletech/hippatextapp/activity/ViewGroupDetails.java
+++ app/src/main/java/com/bluemapletech/hippatextapp/activity/ViewGroupDetails.java
@@ -4,12 +4,15 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,7 +24,9 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +52,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.bluemapletech.hippatextapp.R.layout.view_groupimage_dialog;
+
 public class ViewGroupDetails extends AppCompatActivity{
     private static final String TAG = ViewGroupDetails.class.getCanonicalName();
     private String groupName;
@@ -67,8 +74,13 @@ public class ViewGroupDetails extends AppCompatActivity{
      Groups  groupInformation;
     private String userMailId;
     private String reArrangeEmails;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
     FirebaseUser logged;
     Map<String,String> maps = new HashMap<String,String>();
+    ImageView backPageArrow;
+    String groupValues;
+    String editGroupName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,14 +96,30 @@ public class ViewGroupDetails extends AppCompatActivity{
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         viewImage = (ImageView) findViewById(R.id.view_group_image);
+        ImageView groupNameEdit = (ImageView) findViewById(R.id.groupNameEdit);
         /*
           values comes  from GroupMessageEmployeeActivity
          */
         groupName = getIntent().getStringExtra(GroupMessageEmployeeActivity.groupNames);
+
         /*
           values comes from SelectUser
          */
         groupName = getIntent().getStringExtra(SelectUser.groupNames);
+        if(groupName!=null) {
+             pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+             editor = pref.edit();
+             editor.putString("groupNameValue", groupName);
+             editor.commit();
+        }
+        if(groupName == null){
+            Log.d(TAG,"inside groupName is null");
+            pref = getSharedPreferences("MyPref",MODE_PRIVATE);
+            Log.d(TAG,"groupNamedialogVAlue"+pref.getString("groupNameValue",""));
+            groupName =  pref.getString("groupNameValue","");
+        }
+        TextView name = (TextView) findViewById(R.id.group_name);
+        name.setText(groupName);
 
         fireBaseDatabase = FirebaseDatabase.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
@@ -102,10 +130,12 @@ public class ViewGroupDetails extends AppCompatActivity{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                  String groupValues = snapshot.child("groupName").getValue(String.class);
+                   groupValues = snapshot.child("groupName").getValue(String.class);
                    String groupEmailId = snapshot.child("groupEmailId").getValue(String.class);
                     String randomName = snapshot.child("randomName").getValue(String.class);
                    String  admin = snapshot.child("admin").getValue(String.class);
+
+                    Log.d(TAG,"groupName"+groupName);
                     if(groupValues.matches(groupName)){
                         group.setAdmin(admin);
                         group.setGroupEmailId(groupEmailId);
@@ -128,6 +158,44 @@ public class ViewGroupDetails extends AppCompatActivity{
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+        groupNameEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(ViewGroupDetails.this);
+                alertDialog.setMessage("Enter your group name");
+
+                final EditText input = new EditText(ViewGroupDetails.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                alertDialog.setView(input);
+                alertDialog.setPositiveButton("YES",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                editGroupName = input.getText().toString();
+                                Log.d(TAG,"editGroupName.."+editGroupName);
+                                Log.d(TAG,"0000000000"+groupObj.get(0).getGroupName());
+                                Log.d(TAG,"0000000001"+groupObj.get(1).getGroupName());
+                                for(int i=0;i<groupObj.size();i++){
+                                    String us_mail = groupObj.get(i).getUserMail();
+                                    Log.d(TAG,"us_mail..."+us_mail);
+                                    reArrangeEmails = us_mail.replace(".", "-");
+                                    String r_value = groupObj.get(i).getRandomName();
+                                    DatabaseReference dataReferences = fireBaseDatabase.getReference().child("group").child(reArrangeEmails).child(r_value).child("groupName");
+                                    dataReferences.setValue(editGroupName);
+                                }
+                            }
+                        });
+                alertDialog.setNegativeButton("NO",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                alertDialog.show();
             }
         });
         iv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -155,18 +223,36 @@ public class ViewGroupDetails extends AppCompatActivity{
         });
 
         viewImage.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Dialog dialog=new Dialog(ViewGroupDetails.this,android.R.style.Widget_ProgressBar_Small_Inverse);
                 dialog.setContentView(R.layout.view_groupimage_dialog);
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_header);
+                ImageView backPageArrow = (ImageView) dialog.findViewById(R.id.backarrow);
+                if (toolbar != null) {
+                    setSupportActionBar(toolbar);
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                    dialog.show();
+                }
+                //dialog.addContentView();
                 ImageView showImage = (ImageView) dialog.findViewById(R.id.view_group_img);
                 Picasso.with(ViewGroupDetails.this).load(group.getGroupImage()).fit().centerCrop().into(showImage);
                 dialog.show();
-            }
+                backPageArrow.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Log.d(TAG, "back arrow is clicked!");
+                            groupName = getIntent().getStringExtra(SelectUser.groupNames);
+                            Log.d(TAG, "back arrow is clicked!....."+groupName);
+                            Intent intent = new Intent(getActivity(),ViewGroupDetails.class);
+                            startActivity(intent);
+
+                        }
+                    });
+                }
         });
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.add_group_user_menu, menu);
@@ -237,7 +323,7 @@ public class ViewGroupDetails extends AppCompatActivity{
                    groupInformation.setUserImage(map.get("profilePhoto"));
               // maps.put(separated[l],groupInformation.getUserImage());
                 groupObjs.add(groupInformation);
-                Log.d(TAG,"separated[l]"+separated[l]);
+               Log.d(TAG,"separated[l]"+separated[l]);
                 Log.d(TAG,"group.getRandomName()"+group.getRandomName());
                 getGroupUser(separated[l],group.getRandomName());
                 l++;
@@ -303,7 +389,7 @@ if(info.getStatus().matches("admin") && info.getUserMail().matches(loginMailId))
             mViewHolder.btnName.setText("admin");
             if(info.getUserImage()!=null) {
                 Log.d(TAG,"info.getUserImage()"+info.getUserImage());
-                Picasso.with(context).load(info.getUserImage()).fit().centerCrop().into(mViewHolder.userImage);
+              //  Picasso.with(context).load(info.getUserImage()).fit().centerCrop().into(mViewHolder.userImage);
             }
             return convertView;
         }
